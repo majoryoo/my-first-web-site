abaee2fbf425598ab93167b5d07df4d518a5ddf8
majoryoo <71409859+majoryoo@users.noreply.github.com>
2022-08-16 18:49:18 +0900

    Add files via upload
    .

---
+++ color.js
@@ -0,0 +1,34 @@
+var Body = {
+  SetColor: function (color) {
+    document.querySelector("body").style.color = color;
+  },
+  SetBackgroundColor: function (color) {
+    document.querySelector("body").style.backgroundColor = color;
+  },
+};
+var Links = {
+  SetColor: function (color) {
+    var alist = document.querySelectorAll("a");
+    var i = 0;
+    while (i < alist.length) {
+      alist[i].style.color = color;
+      i += 1;
+    }
+  },
+};
+function nightDayHandler(self) {
+  var target = document.querySelector("body");
+  if (self.value === "night") {
+    Body.SetBackgroundColor("black");
+    Body.SetColor("white");
+    self.value = "day";
+
+    Links.SetColor("powderblue");
+  } else {
+    Body.SetBackgroundColor("white");
+    Body.SetColor("black");
+    self.value = "night";
+
+    Links.SetColor("black");
+  }
+}
